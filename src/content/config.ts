cbc1661ccfc16289b63a8229aaa624358a46539f
djazirifarouk <faroukjaziri1997@outlook.fr>
2025-05-25 14:59:11 +0100

    added Journey tab

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import type Journey from '../pages/journey.astro';
 
 export const collections = {
 	work: defineCollection({
@@ -13,3 +14,17 @@ export const collections = {
 		}),
 	}),
 };
+
+// export const collection = {
+// 	journey: defineCollection({
+// 		type: 'content',
+// 		schema: z.object({
+// 			title: z.string(),
+// 			description: z.string(),
+// 			publishDate: z.coerce.date(),
+// 			tags: z.array(z.string()),
+// 			img: z.string(),
+// 			img_alt: z.string().optional(),
+// 		}),
+// 	}),
+// };
